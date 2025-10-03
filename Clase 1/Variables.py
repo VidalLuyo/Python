fdc5d3ff3c1c46da648cb1b3dfcea1114ba37acd
JesúsVidalLuyo <111780196+VidalLuyo@users.noreply.github.com>
2025-10-03 23:00:24 +0000

    Variables y Operaciones en Python

---
+++ Clase 1/Variables.py
@@ -0,0 +1,18 @@
+# str (cadena de valor) --> "TEXTO" (String)
+# int (entero) --> 10
+# float (números con decimales) --> 10.5
+# bool (booleano) --> True or False
+
+# Formato de las variables
+
+varible = "valor"
+
+##################################################
+
+nombre = "Jesús"
+edad = 25
+ciudad = "Cañete"
+
+print(f"Mi nombre es {nombre}, tengo {edad} años y vivo en {ciudad}")
+
+
